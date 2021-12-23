27389e903a7ed0452b4154e0a5505ecccd1edd57
bbaldaia <brunobfreire98@gmail.com>
2021-12-23 20:42:27 -0300

    Inicializando o projeto em Spring

---
+++ src/main/java/com/caseitau/desafio/controller/DesafioController.java
@@ -0,0 +1,16 @@
+package com.caseitau.desafio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class DesafioController {
+	
+	@GetMapping
+	public String teste() {
+		return "Testando...";
+	}
+
+}
